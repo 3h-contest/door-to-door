68ceed02fb28e362a94ba34c31500232bf1765ae
woo-ah <a01052919770@gmail.com>
2024-08-23 16:55:28 +0900

    wrong

--- pages/screen1.js
+++ pages/screen1.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import {NaverMapView} from "@mj-studio/react-native-naver-map";
 
 const Screen1 = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.HomeText}>Screen1</Text>
-    
+      <NaverMapView style={{flex: 1}} />
     </View>
     
   );
